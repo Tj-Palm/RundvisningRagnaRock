0426c1f0b3cceb84fde9f21520f9bacc38c915a8
Nikolaj Dufke <nikolajdufke@live.dk>
2019-04-26 13:52:16 +0200

    UDScollection. lavet af sarah og nikolaj

---
+++ RundvisningRagnaRock/Collections/UdsCollection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RundvisningRagnaRock.Models;
+
+namespace RundvisningRagnaRock.Collections
+{
+    class UdsCollection
+    {
+
+        #region Instancefields
+
+        private List<UDS> _udsCollection;
+
+        #endregion
+
+        #region Constructor
+
+        public UdsCollection()
+        {
+        _udsCollection = new List<UDS>();
+          
+        }
+        #endregion
+
+        #region Methods
+        public List<UDS> UDScollection
+        {
+            get { return _udsCollection; }
+            private set { _udsCollection = value; }
+        }
+
+        public void Add(UDS uds)
+        {
+            if (uds != null)
+            UDScollection.Add(uds);
+        }
+
+        public void Remove(UDS uds)
+        {
+            if (uds != null)
+            {
+                UDScollection.Remove(uds);
+            }
+        }
+
+        public void Update(UDS uds)
+        {
+            if (uds != null)
+            {
+                foreach (var item in UDScollection)
+                {
+                    if (item.ID == uds.ID)
+                    {
+                        UDScollection[UDScollection.IndexOf(item)] = uds;
+                    }
+                }
+            }
+        }
+        #endregion
+
+    }
+}
