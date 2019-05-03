7fb0caa5bae4ebb6658805dd78cd354b45bde516
Daniel Octavius Palmqvist <dani086m@edu.easj.dk>
2019-05-03 11:11:23 +0200

    Oprettet Employee dictionary

--- RundvisningRagnaRock/ViewModels/LoginPageViewModel.cs
+++ RundvisningRagnaRock/ViewModels/LoginPageViewModel.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RundvisningRagnaRock.Models;
 
 namespace RundvisningRagnaRock.ViewModels
 {
-    class LoginPageViewModel
+    public class LoginPageViewModel
     {
-
+       
     }
 }
