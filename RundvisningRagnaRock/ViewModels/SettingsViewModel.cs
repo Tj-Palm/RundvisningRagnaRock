90c33d3c6f96c5fce56e1e58544e33e5355006b8
Sarah Katrine Minjon Waest <sara693f@edu.easj.dk>
2019-05-07 12:40:08 +0200

    FilePersistency, SettingsViewModel, Changes on SettingsPage

---
+++ RundvisningRagnaRock/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RundvisningRagnaRock.Common;
+
+namespace RundvisningRagnaRock.ViewModels
+{
+    class SettingsViewModel
+    {
+        public SettingsViewModel()
+        {
+            SaveCommand = new RelayCommand(toSaveCommand);
+        }
+
+        public RelayCommand SaveCommand { get; set; }
+
+        public void toSaveCommand()
+        {
+
+        }
+    }
+}
