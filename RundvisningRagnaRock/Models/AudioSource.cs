247a1f7022009ba83e65eae9b0edba675d0a83b1
Sarah Katrine Minjon Waest <sara693f@edu.easj.dk>
2019-05-09 09:26:09 +0200

    Added 2 classes: TextChanger & AudioSource. Updated Settings class

---
+++ RundvisningRagnaRock/Models/AudioSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RundvisningRagnaRock.Models
+{
+    class AudioSource
+    {
+        private double _soundvolume;
+        public double Volume
+        {
+            get { return _soundvolume; }
+            set { _soundvolume = value; }
+        } 
+    }
+}
---
+++ RundvisningRagnaRock/Models/TextChanger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RundvisningRagnaRock.Models
+{
+    class TextChanger
+    {
+        private double _textsize;
+
+        public double textSize
+        {
+            get { return _textsize; }
+            set { _textsize = value; }
+        }
+
+    }
+}
