64b48d76ca9f652a3071d83bd643398254ca6798
Sarah Katrine Minjon Waest <sara693f@edu.easj.dk>
2019-05-09 11:26:03 +0200

    Settings GUI - Save/Binding, Play, Pause, Mute button. Added new song edited settingspage + audiosource + settings

--- RundvisningRagnaRock/Models/AudioSource.cs
+++ RundvisningRagnaRock/Models/AudioSource.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Playback;
+using Windows.UI.Xaml.Controls;
 
 namespace RundvisningRagnaRock.Models
 {
     class AudioSource
     {
         private double _soundvolume;
+        private readonly string musictest = "../Assets/Music/MusicTest.mp3";
+        
         public double Volume
         {
             get { return _soundvolume; }
             set { _soundvolume = value; }
-        } 
+        }
+
+        public AudioSource()
+        {
+            MediaPlayer player = new MediaPlayer();
+            player.Source = player.SetMediaSource(musictest);
+
+
+        }
     }
 }
--- RundvisningRagnaRock/Views/SettingsPage.xaml.cs
+++ RundvisningRagnaRock/Views/SettingsPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using RundvisningRagnaRock.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,15 +25,34 @@ namespace RundvisningRagnaRock.Views
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        MediaPlayer player = new MediaPlayer();
+        
         public SettingsPage()
         {
             this.InitializeComponent();
         }
-
+        //public void Audio()
+        //{
+        //   player.Source = mediasource.
+        //}
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(MainPage));
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            player.IsMuted = true;
+        }
     }
 }
