ef021220b6b31aa8c36846bca822dbba54d9c78a
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2019-05-14 13:19:28 +0200

    Added some instance fields ind mainviewmodel Added Regions too

--- RundvisningRagnaRock/ViewModels/MainViewModel.cs
+++ RundvisningRagnaRock/ViewModels/MainViewModel.cs
@@ -21,14 +21,20 @@ namespace RundvisningRagnaRock.ViewModels
         private readonly string _map2 = "../Assets/Kort1.jpg";
         private readonly string _map3 = "../Assets/Kort2.jpg";
         private ObservableCollection<DynamicButton> _buttons;
-        
+        private List<UDS> _AllUdsCol;
+        private ObservableCollection<UDS> ListviewCol;
+        private Location _SelectedLocation;
+        private Location _udsLocation;
 
+
+
+        #region Constructor
 
         public MainViewModel()
         {
-           
+
             _buttons = new ObservableCollection<DynamicButton>();
-            _buttons.Add(new DynamicButton(115, 50,50,30));
+            _buttons.Add(new DynamicButton(115, 50, 50, 30));
             _buttons.Add(new DynamicButton(33, 123, 36, 48));
 
             Map = _map2;
@@ -38,15 +44,32 @@ namespace RundvisningRagnaRock.ViewModels
             GetUDS = new RelayCommand(toGetUDS);
         }
 
+        #endregion
+
+        #region RelayCommands
+
+        public RelayCommand ChangeToMap2 { get; set; }
+        public RelayCommand ChangeToMap3 { get; set; }
+        public RelayCommand GetUDS { get; set; }
+
+
+        #endregion
+        #region Properties
+
+        public ObservableCollection<DynamicButton> Buttons
+        {
+            get { return _buttons; }
+            set { _buttons = value; }
+        }
+
         public string Map
         {
             get { return _map; }
             set { _map = value; OnPropertyChanged(); }
         }
+        #endregion
 
-        public RelayCommand ChangeToMap2 { get; set; }
-        public RelayCommand ChangeToMap3 { get; set; }
-        public RelayCommand GetUDS { get; set; }
+        #region Methods
 
         private void toChangeMap2()
         {
@@ -55,7 +78,7 @@ namespace RundvisningRagnaRock.ViewModels
 
         private void toChangeMap3()
         {
-            Map = _map3; 
+            Map = _map3;
         }
 
         private void toGetUDS()
@@ -63,18 +86,20 @@ namespace RundvisningRagnaRock.ViewModels
 
         }
 
-        public ObservableCollection<DynamicButton> Buttons
-        {
-            get { return _buttons; }
-            set { _buttons = value; }
-        }
 
         private async Task LoadResourcesAsync()
         {
 
         }
 
+        private void AddLocationSpecificUds()
+        {
+            foreach (var UDS in _AllUdsCol)
+            {
+            }
+        }
 
+        #endregion
 
     }
 }
